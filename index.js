07ad47167f6c98f15547fb66e5fca4901ec291e1
vivek.vikani <vivek.vikani@rakuten.com>
2017-04-20 12:48:48 +0900

    Added timeout when Spotify not running

--- index.js
+++ index.js
@@ -2,69 +2,67 @@ var events = require('events');
 const path = require('path');
 const notifier = require('node-notifier');
 var spotify = require('spotify-node-applescript');
-var isSpotifyRunning
-var initialState
-var isNotified = false
+var isSpotifyRunning;
+var initialState;
+var isNotified = false;
 var trackAlbum, trackAlbumArtist, trackName, artworkUrl, trackPopularity
-var oldTrackName=''
-
+var oldTrackName = '';
 
 var eventEmitter = new events.EventEmitter();
 eventEmitter.setMaxListeners(0);
 
-eventEmitter.on(true, getState)
-eventEmitter.on(false, getRunningStatus)
-eventEmitter.on('state', detectStateChange)
-eventEmitter.on('setTrack', setTrackDetails)
+eventEmitter.on(true, getState);
+eventEmitter.on(false, getRunningStatus);
+eventEmitter.on('state', detectStateChange);
+eventEmitter.on('setTrack', setTrackDetails);
 
-getRunningStatus(getState)
+getRunningStatus(getState);
 
-function getRunningStatus(){
-    spotify.isRunning(function(err, isRunning){
-        if(isRunning){
+function getRunningStatus() {
+    spotify.isRunning(function (err, isRunning) {
+        if (isRunning) {
             eventEmitter.emit(true, detectStateChange);
+        } else {
+            console.log("is running false");
+            setTimeout(function () {
+                eventEmitter.emit(false)
+            }, 5000);
         }
-        else{
-            eventEmitter.emit(false);
-        }
-    }); 
-}
-
-function getState(detectStateChange){        
-    spotify.getState(function(err, state){
-        if(state==null || state == undefined){
-            eventEmitter.emit(false, getState);
-        }
-        else if(state.state == 'playing'){
-            eventEmitter.emit('state', state.state)
-        }
-        else{
-            isNotified = false
-            eventEmitter.emit(true, detectStateChange);
-        } 
     });
 }
 
-function detectStateChange(newState){
+function getState(detectStateChange) {
+    spotify.getState(function (err, state) {
+        if (state === null || state === undefined) {
+            eventEmitter.emit(false, getState);
+        } else if (state.state == 'playing') {
+            eventEmitter.emit('state', state.state)
+        } else {
+            isNotified = false
+            eventEmitter.emit(true, detectStateChange);
+        }
+    });
+}
+
+function detectStateChange(newState) {
     getTrackDetails()
     notify()
     isNotified = true
-    eventEmitter.emit(true, detectStateChange);  // calls getState
+    eventEmitter.emit(true, detectStateChange); // calls getState
 }
 
-function getTrackDetails(){
-    spotify.getTrack(function(err, track){
-        if(track==null || track==undefined){ 
+function getTrackDetails() {
+    spotify.getTrack(function (err, track) {
+        if (track === null || track === undefined) {
             eventEmitter.emit(false, detectStateChange);
-        }
-        else{
-            eventEmitter.emit('setTrack', track.album, track.album_artist, 
+        } else {
+            eventEmitter.emit('setTrack', track.album, track.album_artist,
                 track.name, track.artwork_url, track.popularity)
-        }      
-    });    
+        }
+    });
 }
 
-function setTrackDetails(album, album_artist, name, artwork, popularity){
+function setTrackDetails(album, album_artist, name, artwork, popularity) {
     trackAlbum = album
     trackAlbumArtist = album_artist
     trackName = name
@@ -73,24 +71,24 @@ function setTrackDetails(album, album_artist, name, artwork, popularity){
 }
 
 
-function notify(){
-    if((!isNotified || oldTrackName!=trackName)&&trackName!=undefined){ 
+function notify() {
+    if ((!isNotified || oldTrackName != trackName) && trackName !== undefined) {
 
         notifier.notify({
-            title: trackName,
-            subtitle: trackAlbum,
-            contentImage: path.join(__dirname, '/img/spotify-logo.png'),
-            icon: path.join(__dirname, '/img/play-music-icon.png'),
-            message: trackAlbumArtist + ' ~ Popularity: ' + trackPopularity,
-            sender: 'com.spotify.client',
-            group: 'com.spotify.client',
-            actions: 'Skip'
-        }
-        // ,function() {
-        //     console.log(arguments);
-        // }
+                title: trackName,
+                subtitle: trackAlbum,
+                contentImage: path.join(__dirname, '/img/spotify-logo.png'),
+                icon: path.join(__dirname, '/img/play-music-icon.png'),
+                message: trackAlbumArtist + ' ~ Popularity: ' + trackPopularity,
+                sender: 'com.spotify.client',
+                group: 'com.spotify.client',
+                actions: 'Skip'
+            }
+            // ,function() {
+            //     console.log(arguments);
+            // }
         );
 
         oldTrackName = trackName
-    } 
+    }
 }
