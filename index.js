9eef8edcc12229b882d3036e80c330471592a57e
vivek.vikani <vivek.vikani@rakuten.com>
2017-04-21 16:59:34 +0900

    adding art work from spotify api | not working

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var events = require('events');
 const path = require('path');
 const notifier = require('node-notifier');
 var spotify = require('spotify-node-applescript');
+var request = require('request');
+
 var isSpotifyRunning;
 var initialState;
 var isNotified = false;
-var trackAlbum, trackAlbumArtist, trackName, artworkUrl, trackPopularity
+var trackAlbum, trackAlbumArtist, trackName, artworkUrl, trackPopularity, trackID
 var oldTrackName = '';
 
 var eventEmitter = new events.EventEmitter();
@@ -21,34 +23,45 @@ getRunningStatus(getState);
 function getRunningStatus() {
     spotify.isRunning(function (err, isRunning) {
         if (isRunning) {
-            eventEmitter.emit(true, detectStateChange);
+            console.log("spotify is running");
+            eventEmitter.emit(true);
         } else {
             console.log("is running false");
             setTimeout(function () {
-                eventEmitter.emit(false)
+                eventEmitter.emit(false);
             }, 5000);
         }
-    });
-}
-
-function getState(detectStateChange) {
-    spotify.getState(function (err, state) {
-        if (state === null || state === undefined) {
-            eventEmitter.emit(false, getState);
-        } else if (state.state == 'playing') {
-            eventEmitter.emit('state', state.state)
-        } else {
-            isNotified = false
-            eventEmitter.emit(true, detectStateChange);
+        if (err) {
+            console.log(err);
         }
     });
 }
 
-function detectStateChange(newState) {
-    getTrackDetails()
-    notify()
-    isNotified = true
-    eventEmitter.emit(true, detectStateChange); // calls getState
+function getState() {
+    spotify.getState(function (err, state) {
+        //console.log("emit true: get state");
+        if (state === null || state === undefined) {
+            eventEmitter.emit(false);
+        } else if (state.state == 'playing') {
+            eventEmitter.emit('state');
+        } else {
+            isNotified = false;
+            setTimeout(function () {
+                eventEmitter.emit(true);
+            }, 2000);
+        }
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+function detectStateChange() {
+    //console.log("detect state change");
+    getTrackDetails();
+    notify();
+    isNotified = true;
+    eventEmitter.emit(true); // calls getState
 }
 
 function getTrackDetails() {
@@ -57,38 +70,54 @@ function getTrackDetails() {
             eventEmitter.emit(false, detectStateChange);
         } else {
             eventEmitter.emit('setTrack', track.album, track.album_artist,
-                track.name, track.artwork_url, track.popularity)
+                track.name, track.artwork_url, track.popularity, track.id);
+            //console.log(" track ID   "+track.id);
+        }
+        if (err) {
+            console.log(err);
         }
     });
 }
 
-function setTrackDetails(album, album_artist, name, artwork, popularity) {
-    trackAlbum = album
-    trackAlbumArtist = album_artist
-    trackName = name
-    trackPopularity = popularity
+function setTrackDetails(album, album_artist, name, artwork, popularity, track_id) {
+    trackAlbum = album;
+    trackAlbumArtist = album_artist;
+    trackName = name;
+    trackPopularity = popularity;
+    trackID = track_id;
     // artworkUrl = artwork
 }
 
 
 function notify() {
     if ((!isNotified || oldTrackName != trackName) && trackName !== undefined) {
-
-        notifier.notify({
+        getAlbumArt(function (err, ablumArtUrl) {
+            console.log('Notify Called');
+            notifier.notify({
                 title: trackName,
                 subtitle: trackAlbum,
                 contentImage: path.join(__dirname, '/img/spotify-logo.png'),
-                icon: path.join(__dirname, '/img/play-music-icon.png'),
+                icon: albumArtUrl,
                 message: trackAlbumArtist + ' ~ Popularity: ' + trackPopularity,
                 sender: 'com.spotify.client',
                 group: 'com.spotify.client',
                 actions: 'Skip'
-            }
-            // ,function() {
-            //     console.log(arguments);
-            // }
-        );
-
-        oldTrackName = trackName
+            });
+        });
+        oldTrackName = trackName;
     }
+}
+
+function getAlbumArt() {
+    var arr = trackID.split(":"); //Remove spotify:track form the id
+    trackID = arr[2];
+    //console.log("Track id 2: "+trackID);
+
+    request('https://api.spotify.com/v1/tracks/' + trackID, function (error, response, body) {
+        var body = JSON.parse(body);
+        //console.log('error:', error); // Print the error if one occurred 
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+        console.log('Album art URL:', body.album.images[2].url);
+        return body.album.images[2].url;
+    });
 }
